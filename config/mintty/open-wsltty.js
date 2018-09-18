32bbfd8ca31f25c54664afbc5592d066e89b0eff
Takezoh <take.gn@gmail.com>
2018-09-18 11:42:13 +0900

    fix mintty config for archlinux

--- config/mintty/open-wsltty.js
+++ config/mintty/open-wsltty.js
@@ -2,11 +2,13 @@ var fs = new ActiveXObject("Scripting.FileSystemObject")
 var WshShell = new ActiveXObject("WScript.Shell");
 var env = WshShell.Environment('PROCESS');
 
+var Distro = WScript.Arguments(0)
+
 var Terminal = "%LOCALAPPDATA%\\wsltty\\bin\\mintty.exe"
-var Icon = "-i " + fs.getParentFolderName(WScript.ScriptFullName) + "\\bash.ico"
+var Icon = "-i " + fs.getParentFolderName(WScript.ScriptFullName) + "\\\"" + Distro + ".ico\""
 var Config = "--configdir=" + fs.getParentFolderName(WScript.ScriptFullName)
 var Shell = "/usr/bin/zsh --login"
-var Command = Terminal + " " + Icon + " --WSL= " + Config + " -~ " + Shell
+var Command = Terminal + " " + Icon + " --WSL=\"" + Distro + "\" " + Config + " -~ " + Shell
 
 env.item("SHELL") = "/usr/bin/zsh"
 env.item("WSLENV") = "SHELL/u:PATH/lu:USERPROFILE/pu"
