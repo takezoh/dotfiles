08140aeed7c5b1cf4d88ed507b33c5b85cc80b35
Takehito Gondo <takehito.gondo@gu3.co.jp>
2020-01-29 10:46:33 +0900

    fix mintty

--- config/mintty/open-wsltty.js
+++ config/mintty/open-wsltty.js
@@ -5,13 +5,13 @@ var env = WshShell.Environment('PROCESS');
 var Distro = WScript.Arguments(0)
 
 var Terminal = "%LOCALAPPDATA%\\wsltty\\bin\\mintty.exe"
-var Icon = "-i " + fs.getParentFolderName(WScript.ScriptFullName) + "\\\"" + Distro + ".ico\""
-var Config = "--configdir=" + fs.getParentFolderName(WScript.ScriptFullName)
+var Icon = fs.getParentFolderName(WScript.ScriptFullName) + "\\\"" + Distro + ".ico\""
+var Config = fs.getParentFolderName(WScript.ScriptFullName)
 var Shell = "/usr/bin/zsh --login"
-var Command = Terminal + " " + Icon + " --WSL=\"" + Distro + "\" " + Config + " -~ " + Shell
+var Command = Terminal + " -i " + Icon + " --WSL=\"" + Distro + "\" --configdir=" + Config + " -~ " + Shell
 
 env.item("SHELL") = "/usr/bin/zsh"
-env.item("WSL_SHELL") = "/usr/bin/zsh"
-env.item("WSLENV") = "SHELL/u:WSL_SHELL/u:PATH/lu:USERPROFILE/pu:LOCALAPPDATA/pu:PROGRAMFILES/pu:ANDROID_HOME/pu:NDK_ROOT/pu"
+env.item("WSLSHELL") = "/usr/bin/zsh"
+env.item("WSLENV") = "SHELL/u:WSLSHELL/u:PATH/lu:USERPROFILE/pu:APPDATA/pu:LOCALAPPDATA/pu:PROGRAMFILES/pu:ANDROID_HOME/pu:NDK_ROOT/pu"
 
 WshShell.Run("cmd /c start " + Command, 0);
