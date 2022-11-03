ce5180c43ae52866de451345d795eb40de8a450e
Takehito Gondo <takehito.gondo@thirdverse.co.jp>
2022-11-04 01:16:09 +0900

    fix wsltty

--- misc/install/install-terminal-launcher-windows.js
+++ misc/install/install-terminal-launcher-windows.js
@@ -1,17 +1,23 @@
 var fs = WScript.CreateObject("Scripting.FileSystemObject")
 var WshShell = WScript.CreateObject("WScript.Shell")
-var RootDir = fs.getParentFolderName(WScript.ScriptFullName) + "\\..\\..\\"
-var WslDir = RootDir + "\\config\\mintty\\"
+var RootDir = fs.getParentFolderName(WScript.ScriptFullName) + "\\..\\.."
+var WslDir = RootDir + "\\config\\mintty"
+
 
 function MakeTerminalLink(Name) {
   var TempPath = fs.GetSpecialFolder(2) + "\\Mintty - " + Name + ".lnk"
   var lnk = WshShell.CreateShortcut(TempPath)
-  lnk.TargetPath = WslDir + "open-wsltty.vbs"
-  lnk.Arguments = "\"" + Name + "\""
-  lnk.IconLocation = WslDir + Name + ".ico"
+  var Terminal = "%LOCALAPPDATA%\\wsltty\\bin\\mintty.exe"
+  var Icon = WslDir + "\\" + Name + ".ico"
+  var Shell = "/usr/bin/zsh --login"
+  //var Command = " -i \"" + Icon + "\" --WSL=\"" + Name + "\" --configdir=\"" + WslDir + "\" -~ " + Shell
+  var Command = " -i \"" + Icon + "\" --WSL= --configdir=\"" + WslDir + "\" -~ " + Shell
+  lnk.TargetPath = Terminal
+  lnk.Arguments = Command
+  lnk.IconLocation = Icon
   lnk.Save()
   WshShell.Run("cmd /c move \"" + TempPath + "\" \"%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\\"", 0)
 }
 
-MakeTerminalLink("Ubuntu-18.04")
-MakeTerminalLink("ArchLinux")
+MakeTerminalLink("Ubuntu")
+//MakeTerminalLink("ArchLinux")
