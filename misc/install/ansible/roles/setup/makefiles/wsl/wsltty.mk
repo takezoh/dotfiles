40b25118b6cdcfe45baf760b5e78218c5d3f7b00
Takehito Gondo <takehito.gondo@thirdverse.co.jp>
2023-01-16 18:14:38 -0800

    fix wsltty installer

--- misc/install/ansible/roles/setup/makefiles/wsl/wsltty.mk
+++ misc/install/ansible/roles/setup/makefiles/wsl/wsltty.mk
@@ -2,8 +2,9 @@ DOWNLOAD_DIR := $(shell wslpath -au `cmd.exe /d /c echo %USERPROFILE%/Downloads`
 INSTALL_PREFIX := $(shell wslpath -au `cmd.exe /d /c echo %LOCALAPPDATA%/wsltty`)
 EXECUTABLE := $(INSTALL_PREFIX)/bin/mintty.exe
 
-VERSION := 3.6.0-x86-64
-INSTALLER := "wsltty-$(VERSION)-install.exe"
+VERSION := 3.6.1.2
+ARCH=x86_64
+INSTALLER := wsltty-$(VERSION)-$(ARCH)-install.exe
 INSTALLER_URL := "https://github.com/mintty/wsltty/releases/download/$(VERSION)/$(INSTALLER)"
 
 all: $(EXECUTABLE)
