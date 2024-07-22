6acb7e00678d17542d71b06b681dd92dd61fa183
Takehito Gondo <takehito.gondo@thirdverse.co.jp>
2024-07-22 13:16:50 +0900

    fix install

---
+++ misc/install/scripts/30_terminal/windows.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+set -ex
+
+windowsterminal_package_path=$(powershell.exe Write-Host -NoNewline '$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe')
+windowsterminal_config_path=$(wslpath -aw $ROOTDIR/config/WindowsTerminal)
+
+cmd.exe /d /c rmdir /q /s $windowsterminal_package_path'\LocalState' || true
+cmd.exe /d /c mklink /d $windowsterminal_package_path'\LocalState' $windowsterminal_config_path
--- misc/install/scripts/90_install/before.sh
+++ misc/install/scripts/90_install/before.sh
@@ -11,3 +11,6 @@ common_makefiles=(
 for mk in ${common_makefiles[@]}; do
 	BINDIR=/usr/local/bin ROOT_DIRECTORY=$ROOTDIR make -f $ROOTDIR/misc/install/scripts/90_install/makefiles/$mk.mk -C /usr/local/opt
 done
+
+# rust cargo
+curl https://sh.rustup.rs -sSf | sh
